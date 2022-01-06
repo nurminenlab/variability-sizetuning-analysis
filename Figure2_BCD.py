10cf7d57c7f9a59b27c18609456bf8d62fd1e330
Lauri <lonurmin@central.uh.edu>
2022-01-06 16:35:17 -0600

    added swarmplot for basline differences in quenchers and amplifiers

--- Figure2_BCD.py
+++ Figure2_BCD.py
@@ -1,21 +1,22 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 import statsmodels.api as sm
+from statsmodels.formula.api import ols
+import seaborn as sns
 
 amplification_DF = pd.read_csv('amplification_DF.csv') 
 
-plt.figure(2, figsize=(4.5, 1.5))
-ax = plt.subplot(1,2,2)
-SEM = amplification_DF.groupby(['layer','qtype_signi'])['bsl'].sem()
-amplification_DF.groupby(['layer','qtype_signi'])['bsl'].mean().plot(kind='bar',yerr=SEM,ax=ax)
 
-lm = ols('bsl ~ C(qtype_signi) + C(layer)',data=quencher_DF).fit()
+#plt.figure(1, figsize=(4.5, 1.5))
+SEM = amplification_DF.groupby(['layer','qtype_signi'])['bsl'].sem()
+amplification_DF.groupby(['layer','qtype_signi'])['bsl'].mean().plot(kind='bar',yerr=SEM)
+
+lm = ols('bsl ~ C(qtype_signi) + C(layer)',data=amplification_DF).fit()
 table = sm.stats.anova_lm(lm,typ=1)
 
-SEM = amplification_DF.groupby('layer')['maxquench_diam','maxamplif_diam'].sem()
-amplification_DF.groupby('layer')['maxquench_diam','maxamplif_diam'].mean().plot(kind='bar',yerr=SEM)
+SEM = amplification_DF.groupby('layer')[['maxquench_diam','maxamplif_diam']].sem()
+amplification_DF.groupby('layer')[['maxquench_diam','maxamplif_diam']].mean().plot(kind='bar',yerr=SEM)
 
-plt.figure(figsize=(1.481, 1.128))
-ax = plt.subplot(1,1,1)
-SEM = amplification_DF.groupby('layer')['maxquench','maxamplif'].sem()
-amplification_DF.groupby('layer')['maxquench','maxamplif'].mean().plot(kind='bar',yerr=SEM,ax=ax)
+#plt.figure(figsize=(1.481, 1.128))
+SEM = amplification_DF.groupby('layer')[['maxquench','maxamplif']].sem()
+amplification_DF.groupby('layer')[['maxquench','maxamplif']].mean().plot(kind='bar',yerr=SEM)
