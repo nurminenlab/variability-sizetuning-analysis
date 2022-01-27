c2f9e48f576e090350c2492c88e8fb3964f29435
hlovn <lonurmin@central.uh.edu>
2022-01-27 11:53:37 -0600

    added stats

--- Figure1D.py
+++ Figure1D.py
@@ -1,8 +1,9 @@
-import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import scipy.stats as sts
 import seaborn as sns
+import statsmodels.api as sm
+from statsmodels.formula.api import ols
 
 F_dir   = 'C:/Users/lonurmin/Desktop/CorrelatedVariability/results/paper_v9/MK-MU/'
 params = pd.read_csv(F_dir + 'extracted_params-Dec-2021.csv')
@@ -10,8 +11,7 @@ params = pd.read_csv(F_dir + 'extracted_params-Dec-2021.csv')
 FF_size = pd.DataFrame(columns=['fano','size','layer'])
 FF_size_all = pd.DataFrame(columns=['fano','size','layer'])
 
-indx = params[params['fit_fano_MIN'] == 0].index
-params.drop(indx,inplace=True)
+
 
 FF_RF = pd.DataFrame(data={'fano':params['fit_fano_RF'].values,'size':['RF']*len(params.index),'layer':params['layer'].values})
 FF_LAR = pd.DataFrame(data={'fano':params['fit_fano_LAR'].values,'size':['LAR']*len(params.index),'layer':params['layer'].values})
@@ -37,3 +37,16 @@ FF_size_all = FF_size_all.append(FF_SML)
 FF_size_all = FF_size_all.append(FF_RF)
 FF_size_all = FF_size_all.append(FF_SUR)
 FF_size_all = FF_size_all.append(FF_LAR)
+
+print('\n ANOVA for the main effect of size')
+lm = ols('fano ~ C(size)',data=FF_size_all).fit()
+table = sm.stats.anova_lm(lm,typ=1)
+print(table)
+
+print('\n t-test for fano-factor in SML vs RF across layers')
+print(params.groupby('layer').apply(lambda df: sts.ttest_ind(df['fit_fano_SML'],df['fit_fano_RF'],nan_policy='omit')))
+
+print('\n t-test for fano-factor in RF vs LAR across layers')
+print(params.groupby('layer').apply(lambda df: sts.ttest_ind(df['fit_fano_LAR'],df['fit_fano_RF'],nan_policy='omit')))
+
+sts.f_oneway()
