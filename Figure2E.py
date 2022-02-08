597157d336ee76ae401ba2c421959ee1a0a7db1b
hlovn <lonurmin@central.uh.edu>
2022-02-08 11:41:08 -0600

    save plots

--- Figure2E.py
+++ Figure2E.py
@@ -4,13 +4,15 @@ import pandas as pd
 from statsmodels.formula.api import ols
 import statsmodels.api as sm
 
+fig_dir   = 'C:/Users/lonurmin/Desktop/CorrelatedVariability/results/paper_v9/IntermediateFigures/'
+
 # to generate quencher_DF run generate_quencher_DF.py
 quencher_DF = pd.read_csv('quencher_DF.csv')
 
 # bar graph of the proportion of quenchers in each layer
 ax = plt.subplot(111)
 quencher_DF.groupby(['layer','FF_sup']).size().groupby(level=0).apply(lambda x: 100 * x / x.sum()).unstack().plot(kind='bar', stacked=True, ax=ax,color=['red','grey','blue'])
-
+plt.savefig(fig_dir + 'F2E.svg',bbox_inches='tight',pad_inches=0)
 # the proportion of quenchers 
 print('\n The proportion of quenchers:')
 print(quencher_DF.groupby(['FF_sup']).size() / len(quencher_DF))
