9ad0bd974dc1e1a8d8b9aaab5a81bb9eff5258bc
hlovn <lonurmin@central.uh.edu>
2022-02-08 17:20:33 -0600

    added outlier detection

--- Figure2_BCD.py
+++ Figure2_BCD.py
@@ -5,6 +5,9 @@ from statsmodels.formula.api import ols
 import seaborn as sns
 import scipy.stats as sts
 import numpy as np
+import sys
+sys.path.append('C:/Users/lonurmin/Desktop/code/DataAnalysis/')
+import data_analysislib as dalib
 
 amplification_DF = pd.read_csv('amplification_DF_division.csv')
 n_boots = 1000
@@ -14,23 +17,23 @@ SEM = amplification_DF.groupby(['layer','qtype_signi'])['bsl'].sem()
 amplification_DF.groupby(['layer','qtype_signi'])['bsl'].mean().plot(kind='bar',yerr=SEM)
 
 plt.figure()
-sns.swarmplot(x='layer',y='bsl',data=amplification_DF,hue='qtype_signi')
+sns.stripplot(x='layer',y='bsl',data=amplification_DF,hue='qtype_signi',dodge=True)
 
 lm = ols('bsl ~ C(qtype_signi) + C(layer)',data=amplification_DF).fit()
 table = sm.stats.anova_lm(lm,typ=1)
 
 plt.figure()
 sns.barplot(y='bsl',x='layer',data=amplification_DF,alpha=0.5)
-sns.swarmplot(y='bsl',x='layer',data=amplification_DF)
+sns.stripplot(y='bsl',x='layer',data=amplification_DF)
 
 plt.figure()
 sns.barplot(y='bsl',x='qtype_signi',data=amplification_DF,fc='gray')
-sns.swarmplot(y='bsl',x='qtype_signi',data=amplification_DF,alpha=0.5)
+sns.stripplot(y='bsl',x='qtype_signi',data=amplification_DF,alpha=0.5)
 
 plt.figure()
 ax = plt.subplot(1,2,1)
-sns.swarmplot(x='layer',y='maxquench_diam',data=amplification_DF,color='blue',ax=ax)
-sns.swarmplot(x='layer',y='maxamplif_diam',data=amplification_DF,color='orange',ax=ax)
+sns.stripplot(x='layer',y='maxquench_diam',data=amplification_DF,color='blue',ax=ax)
+sns.stripplot(x='layer',y='maxamplif_diam',data=amplification_DF,color='orange',ax=ax)
 ax.set_yscale('log')
 
 # stimulus diameter relative to RF at maxquench / maxmplif 
@@ -99,8 +102,8 @@ ax.set_xticklabels(['SG','G','IG'])
 
 plt.figure()
 ax = plt.subplot(1,2,1)
-sns.swarmplot(x='layer',y='RFnormed_maxquench_diam',data=amplification_DF,color='blue',ax=ax)
-sns.swarmplot(x='layer',y='RFnormed_maxamplif_diam',data=amplification_DF,color='orange',ax=ax)
+sns.stripplot(x='layer',y='RFnormed_maxquench_diam',data=amplification_DF,color='blue',ax=ax)
+sns.stripplot(x='layer',y='RFnormed_maxamplif_diam',data=amplification_DF,color='orange',ax=ax)
 ax.set_yscale('log')
 
 ax = plt.subplot(1,2,2)
@@ -111,12 +114,19 @@ ax.set_xticks([1,3,5])
 ax.set_xticklabels(['SG','G','IG'])
 
 plt.figure()
-ax = plt.subplot(1,2,2)
+ax = plt.subplot(1,3,1)
+sns.stripplot(x='layer',y='maxquench',data=amplification_DF,color='blue',ax=ax)
+sns.stripplot(x='layer',y='maxamplif',data=amplification_DF,color='orange',ax=ax)
+
+ax = plt.subplot(1,3,2)
 SEM = amplification_DF.groupby('layer')[['maxquench','maxamplif']].sem()
-amplification_DF.groupby('layer')[['maxquench','maxamplif']].mean().plot(kind='bar',yerr=SEM)
+amplification_DF.groupby('layer')[['maxquench','maxamplif']].mean().plot(kind='bar',yerr=SEM,ax=ax)
 
+plt.figure()
+ax = plt.subplot(1,1,1)
+SEM = amplification_DF[['maxquench','maxamplif']].sem()
+amplification_DF[['maxquench','maxamplif']].mean().plot(kind='bar',yerr=SEM,ax=ax)
 
-ax = plt.subplot(1,2,1)
-sns.swarmplot(x='layer',y='maxquench',data=amplification_DF,color='blue',ax=ax)
-sns.swarmplot(x='layer',y='maxamplif',data=amplification_DF,color='orange',ax=ax)
-#ax.set_yscale('log')
+DF = amplification_DF
+DF['maxamplif_olRem'] = DF.groupby('layer')[['maxamplif']].apply(dalib.outlier, args=(DF['maxamplif'].median(),DF['maxamplif'].mad()))
+
